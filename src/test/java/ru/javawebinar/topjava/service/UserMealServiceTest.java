b26beb755d7710072a2dec6b9ff36823ebe079d6
ilya <iliahoo.pr@gmail.com>
2016-04-02 17:31:01 +0300

    mavenAndSpringProfiles
    (cherry picked from commit 12c184f38ec6c1129624be3deedf244c49296276)
    
    # Conflicts:
    #	src/main/resources/spring/spring-db.xml

--- src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
@@ -3,11 +3,13 @@ package ru.javawebinar.topjava.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -25,6 +27,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(Profiles.HSQLDB)
 public class UserMealServiceTest {
 
     @Autowired
